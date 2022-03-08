c3c250da87f97f04bd40234af13d76470480b0a8
Seth Michael Larson <seth.larson@elastic.co>
2022-03-08 11:55:31 -0600

    Add support for MemoryBIO's and aiohttp

--- test_truststore.py
+++ test_truststore.py
@@ -2,6 +2,8 @@ import platform
 import socket
 import ssl
 
+import aiohttp
+import aiohttp.client_exceptions
 import pytest
 import urllib3
 import urllib3.exceptions
@@ -9,16 +11,21 @@ import urllib3.exceptions
 from truststore import TruststoreSSLContext
 
 successful_hosts = pytest.mark.parametrize("host", ["example.com", "1.1.1.1"])
+
+failure_hosts_list = [
+    "wrong.host.badssl.com",
+    "expired.badssl.com",
+    "self-signed.badssl.com",
+    "untrusted-root.badssl.com",
+    "superfish.badssl.com",
+]
+
+if platform.system() != "Linux":
+    failure_hosts_list.append("revoked.badssl.com")
+
 failure_hosts = pytest.mark.parametrize(
     "host",
-    [
-        "wrong.host.badssl.com",
-        "expired.badssl.com",
-        "self-signed.badssl.com",
-        "untrusted-root.badssl.com",
-        "revoked.badssl.com",
-        "superfish.badssl.com",
-    ],
+    failure_hosts_list,
 )
 
 
@@ -47,10 +54,22 @@ def test_failures(host):
 
 @successful_hosts
 def test_sslcontext_api_success(host):
-    http = urllib3.PoolManager(ssl_context=TruststoreSSLContext())
+    ctx = TruststoreSSLContext()
+    http = urllib3.PoolManager(ssl_context=ctx)
     resp = http.request("GET", f"https://{host}")
     assert resp.status == 200
     assert len(resp.data) > 0
+
+
+@successful_hosts
+@pytest.mark.asyncio
+async def test_sslcontext_api_success_async(host):
+    ctx = TruststoreSSLContext()
+    async with aiohttp.ClientSession() as http:
+        resp = await http.request("GET", f"https://{host}", ssl=ctx)
+
+        assert resp.status == 200
+        assert len(await resp.text()) > 0
 
 
 @failure_hosts
@@ -58,8 +77,25 @@ def test_sslcontext_api_failures(host):
     if platform.system() == "Linux" and host == "revoked.badssl.com":
         pytest.skip("Linux currently doesn't support CRLs")
 
+    ctx = TruststoreSSLContext()
+    http = urllib3.PoolManager(ssl_context=ctx)
     with pytest.raises(urllib3.exceptions.SSLError) as e:
-        http = urllib3.PoolManager(ssl_context=TruststoreSSLContext())
         http.request("GET", f"https://{host}", retries=False)
+
+    assert "cert" in repr(e.value).lower() and "verif" in repr(e.value).lower()
+
+
+@failure_hosts
+@pytest.mark.asyncio
+async def test_sslcontext_api_failures_async(host):
+    if platform.system() == "Linux" and host == "revoked.badssl.com":
+        pytest.skip("Linux currently doesn't support CRLs")
+
+    ctx = TruststoreSSLContext()
+    async with aiohttp.ClientSession() as http:
+        with pytest.raises(
+            aiohttp.client_exceptions.ClientConnectorCertificateError
+        ) as e:
+            await http.request("GET", f"https://{host}", ssl=ctx)
 
     assert "cert" in repr(e.value).lower() and "verif" in repr(e.value).lower()
