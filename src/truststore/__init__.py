89f08e2aa90bae1ed0e01450e2603822289ec694
Seth Michael Larson <seth@python.org>
2024-08-14 14:37:16 -0500

    Detect API availability with MemoryBIO

--- src/truststore/__init__.py
+++ src/truststore/__init__.py
@@ -1,5 +1,6 @@
 """Verify certificates using native system trust stores"""
 
+import ssl
 import sys as _sys
 
 if _sys.version_info < (3, 10):
@@ -12,12 +13,22 @@ try:
 except ImportError:
     raise ImportError("truststore requires the 'ssl' module")
 else:
-    if not hasattr(_ssl.SSLSocket, "get_unverified_chain"):
-        raise ImportError("truststore requires peer certificate APIs to be available")
+    _sslmem = _ssl.MemoryBIO()
+    _sslobj = _ssl.create_default_context().wrap_bio(
+        _sslmem,
+        _sslmem,
+    )
+    try:
+        while not hasattr(_sslobj, "get_unverified_chain"):
+            _sslobj = _sslobj._sslobj  # type: ignore[attr-defined]
+    except AttributeError:
+        raise ImportError(
+            "truststore requires peer certificate APIs to be available"
+        ) from None
 
 from ._api import SSLContext, extract_from_ssl, inject_into_ssl  # noqa: E402
 
-del _api, _sys, _ssl  # type: ignore[name-defined] # noqa: F821
+del _api, _sys, _ssl, _sslobj, _sslmem  # type: ignore[name-defined] # noqa: F821
 
 __all__ = ["SSLContext", "inject_into_ssl", "extract_from_ssl"]
 __version__ = "0.9.2"
