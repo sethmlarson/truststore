985c047c6325a841dba08a23eefa8504be361d5d
Seth Michael Larson <seth@python.org>
2024-08-19 15:20:04 -0500

    Remove unnecessary import, only check 3.12 and earlier

--- src/truststore/__init__.py
+++ src/truststore/__init__.py
@@ -1,30 +1,30 @@
 """Verify certificates using native system trust stores"""
 
-import ssl
 import sys as _sys
 
 if _sys.version_info < (3, 10):
     raise ImportError("truststore requires Python 3.10 or later")
 
 # Detect Python runtimes which don't implement SSLObject.get_unverified_chain() API
-# This API only became public in CPython 3.13 but was available in CPython since 3.10.
-try:
-    import ssl as _ssl
-except ImportError:
-    raise ImportError("truststore requires the 'ssl' module")
-else:
-    _sslmem = _ssl.MemoryBIO()
-    _sslobj = _ssl.create_default_context().wrap_bio(
-        _sslmem,
-        _sslmem,
-    )
+# This API only became public in Python 3.13 but was available in CPython and PyPy since 3.10.
+if _sys.version_info < (3, 13):
     try:
-        while not hasattr(_sslobj, "get_unverified_chain"):
-            _sslobj = _sslobj._sslobj  # type: ignore[attr-defined]
-    except AttributeError:
-        raise ImportError(
-            "truststore requires peer certificate APIs to be available"
-        ) from None
+        import ssl as _ssl
+    except ImportError:
+        raise ImportError("truststore requires the 'ssl' module")
+    else:
+        _sslmem = _ssl.MemoryBIO()
+        _sslobj = _ssl.create_default_context().wrap_bio(
+            _sslmem,
+            _sslmem,
+        )
+        try:
+            while not hasattr(_sslobj, "get_unverified_chain"):
+                _sslobj = _sslobj._sslobj  # type: ignore[attr-defined]
+        except AttributeError:
+            raise ImportError(
+                "truststore requires peer certificate chain APIs to be available"
+            ) from None
 
 from ._api import SSLContext, extract_from_ssl, inject_into_ssl  # noqa: E402
 
