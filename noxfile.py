0c58b2f03ad9ce0d774acf08c7c5a00549c746b4
Seth Michael Larson <seth@python.org>
2025-07-08 14:58:18 -0500

    Fix CI for macOS and pytest, add pre-commit

--- noxfile.py
+++ noxfile.py
@@ -1,48 +1,11 @@
-import glob
 import os
 
 import nox
 
-SOURCE_PATHS = glob.glob("*.py") + ["src/"] + ["tests/"]
-SOURCE_FILES = (
-    glob.glob("*.py")
-    + glob.glob("src/**/*.py", recursive=True)
-    + glob.glob("tests/**/*.py", recursive=True)
-)
 # Run in the current Python environment on CI
 # (where there's a matrix of jobs using different Pythons)
 # but test multiple versions locally
-PYTHONS = None if os.environ.get("CI") else ["3.10", "3.11", "3.12", "3.13"]
-
-
-@nox.session
-def format(session):
-    session.install("black", "isort", "pyupgrade", "tomli")
-    session.run("black", *SOURCE_PATHS)
-    session.run("isort", "--profile=black", *SOURCE_PATHS)
-    session.run(
-        "pyupgrade", "--py37-plus", "--exit-zero-even-if-changed", *SOURCE_FILES
-    )
-
-    lint(session)
-
-
-@nox.session
-def lint(session):
-    session.install(
-        "black", "isort", "flake8", "mypy", "types-certifi", "tomli", "urllib3"
-    )
-    session.run("flake8", "--ignore=E501,W503,E704", *SOURCE_PATHS)
-    session.run("black", "--check", *SOURCE_PATHS)
-    session.run("isort", "--check", "--profile=black", *SOURCE_PATHS)
-    session.run(
-        "mypy",
-        "--strict",
-        "--show-error-codes",
-        "--install-types",
-        "--non-interactive",
-        "src/",
-    )
+PYTHONS = None if os.environ.get("CI") else ["3.10", "3.11", "3.12", "3.13", "3.14"]
 
 
 @nox.session(python=PYTHONS)
